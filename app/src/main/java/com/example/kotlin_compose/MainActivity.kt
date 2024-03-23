d3987ba25b35136cbc7e18c539e27ad5b7aecb77
mduc-dev <duc040301@gmail.com>
2024-03-23 21:10:44 +0700

    inital

---
+++ app/src/main/java/com/example/kotlin_compose/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.kotlin_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kotlin_compose.ui.theme.Kotlin_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Kotlin_composeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            BottomTabNavigator()
+        },
+    ) { padding ->
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomPreview() {
+    Kotlin_composeTheme {
+        HomeScreen()
+    }
+}
