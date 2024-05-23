cac2c422221bbf9ee62ae86841eb089ac9eec7c8
mduc-dev <duc040301@gmail.com>
2024-05-23 23:05:47 +0700

    feat: replace button with filter chip

--- app/src/main/java/com/example/kotlin_compose/presentation/screens/account/Account.kt
+++ app/src/main/java/com/example/kotlin_compose/presentation/screens/account/Account.kt
@@ -1,61 +1,70 @@
 package com.example.kotlin_compose.presentation.screens.account
 
-import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
-import com.example.kotlin_compose.presentation.components.NoExistData
-import com.example.kotlin_compose.ui.theme.PPNeu
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.composed
-import com.example.kotlin_compose.presentation.components.CustomButton
-import com.example.kotlin_compose.ui.theme.BlackDisable
-import kotlinx.coroutines.launch
-
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.*
+import androidx.compose.ui.unit.sp
 import com.example.kotlin_compose.R
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalDensity
+import com.example.kotlin_compose.presentation.components.NoExistData
 import com.example.kotlin_compose.presentation.utils.isEmpty
+import com.example.kotlin_compose.ui.theme.Black1A
+import com.example.kotlin_compose.ui.theme.BlackDisable
+import com.example.kotlin_compose.ui.theme.BlackF3
+import com.example.kotlin_compose.ui.theme.Green1a
 import com.example.kotlin_compose.ui.theme.Green31
+import com.example.kotlin_compose.ui.theme.PPNeu
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
 
-val listButton = listOf("All", "Gamelists", "Articles", "Videos")
 val tabs = listOf("Posts", "Saved", "Drafts")
+val enumValuesChip = listOf("All", "Gamelists", "Articles", "Videos")
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -72,7 +81,7 @@ fun Account() {
         tabWidthStateList
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    Scaffold(containerColor = Black1A) { padding ->
         LazyColumn(
             state = listState,
             modifier = Modifier.fillMaxSize(),
@@ -83,7 +92,7 @@ fun Account() {
                     selectedTabIndex = pagerState.currentPage, edgePadding = 20.dp,
                     containerColor = Color.Transparent,
                     divider = {},
-                    modifier = Modifier.padding(vertical = 16.dp),
+                    modifier = Modifier.padding(top = 16.dp,bottom = 8.dp),
                     indicator = { tabPositions ->
                         TabRowDefaults.SecondaryIndicator(
                             modifier = Modifier
@@ -91,7 +100,7 @@ fun Account() {
                                     currentTabPosition = tabPositions[pagerState.currentPage],
                                     tabWidth = tabWidths[pagerState.currentPage]
                                 )
-                                .clip(shape = RoundedCornerShape(10.dp)), color = Green31
+                                .clip(shape = MaterialTheme.shapes.large), color = Green31
                         )
                     },
                 ) {
@@ -122,39 +131,66 @@ fun Account() {
                     }
                 }
                 PageContent(pagerState)
-
             }
         }
     }
-
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PageContent(pagerState: PagerState) {
     val styleTextBtn: TextStyle = MaterialTheme.typography.titleMedium.copy(
-        color = Color.White, fontFamily = PPNeu, fontWeight = FontWeight.Bold
+        fontFamily = PPNeu, fontWeight = FontWeight.Bold
     )
+    var selectedFilter by remember { mutableStateOf(enumValuesChip.first()) }
+
     HorizontalPager(
         state = pagerState, beyondBoundsPageCount = 10
     ) { page ->
-        Column(modifier = Modifier.fillMaxSize()) {
-            CustomButton(
-                onPress = {},
-                children = {
-                    Text(
-                        "All", color = Color.White, style = styleTextBtn
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 6.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                enumValuesChip.forEach {
+                    FilterChip(
+                        selected = it == selectedFilter,
+                        onClick = {
+                            selectedFilter = it
+                        },
+                        label = { Text(text = it, style = styleTextBtn) },
+                        interactionSource = DisabledInteractionSource(),
+                        colors = FilterChipDefaults.filterChipColors(
+                            selectedContainerColor = Green1a,
+                            labelColor = BlackDisable,
+                            selectedLabelColor = Color.White,
+                            containerColor = Black1A,
+
+                        ),
+                        border = FilterChipDefaults.filterChipBorder(
+                            enabled = true,
+                            selected = it == selectedFilter,
+                            borderColor = BlackF3,
+                            selectedBorderColor = Color.Transparent,
+                            selectedBorderWidth = 0.dp,
+
+                        ),
+                        shape = MaterialTheme.shapes.small
                     )
-                },
-            )
-            if(isEmpty("123")){
+
+                }
+
+            }
+            if (isEmpty("")) {
                 NoExistData(
                     subTextNull = "Write a post to start your profile’s never-ending journey.",
                     painterResourceName = R.drawable.sad_icon_top,
                     stringResourceName = R.string.sad_icon_top
                 )
             } else {
-             Text(text = "content list here", color = Color.White)
+                Text(text = "content list here", color = Color.White)
             }
         }
     }
--- app/src/main/java/com/example/kotlin_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kotlin_compose/ui/theme/Theme.kt
@@ -61,7 +61,7 @@ fun Kotlin_composeTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
