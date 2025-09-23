2f33714c7fbb9cbcd4b85da3813a99628fa8f3ed
manhduckimei <ducnmanh@kimei.vn>
2025-09-23 22:45:39 +0700

    Fix status bar color in dark theme
    The status bar color was not changing correctly when switching to dark theme. This commit fixes the issue by inverting the `isAppearanceLightStatusBars` flag when dark theme is active.
    
    Additionally, the `Kotlin_composeTheme` was removed from `TapMain.kt` as it's already applied at a higher level in the component tree.

--- app/src/main/java/com/example/kotlin_compose/presentation/navigation/TapMain.kt
+++ app/src/main/java/com/example/kotlin_compose/presentation/navigation/TapMain.kt
@@ -3,19 +3,16 @@ package com.example.kotlin_compose.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.rememberNavController
-import com.example.kotlin_compose.ui.theme.Kotlin_composeTheme
 import org.koin.compose.koinInject
 
 @Composable
 fun TapMain() {
     val composeNavigator: AppComposeNavigator = koinInject()
-    Kotlin_composeTheme {
-        val navHostController = rememberNavController()
+    val navHostController = rememberNavController()
 
-        LaunchedEffect(Unit) {
-            composeNavigator.handleNavigationCommands(navHostController)
-        }
-
-        TapNavHost(navHostController, composeNavigator)
+    LaunchedEffect(Unit) {
+        composeNavigator.handleNavigationCommands(navHostController)
     }
+
+    TapNavHost(navHostController, composeNavigator)
 }
